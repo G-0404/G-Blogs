81b2ff7b74693ede6f84d883b9135445f3a0e83b
WeiSheng <842469165@qq.com>
2024-04-04 16:20:20 +0800

    update:更新组件化调用

---
+++ src/views/BlogsHomePage/components/MyInfoMenuBar.vue
@@ -0,0 +1,89 @@
+<!--
+ * @Author: 微生
+ * @Date: 2024-04-04 16:14:17
+ * @LastEditors: WeiSheng 842469165@qq.com
+ * @LastEditTime: 2024-04-04 16:19:21
+ * @FilePath: /blogs-G/src/views/BlogsHomePage/components/MyInfoMenuBar.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by 微生, All Rights Reserved. 
+-->
+<template>
+  <div class="my-info-menu-bar-container transform-55">
+    <div class="menu-top"></div>
+    <div class="menu-center">
+      <div class="menu-item" :class="{ 'act-item': active_index === index }" v-for="(item, index) in 20">
+        首页{{ item }}
+      </div>
+    </div>
+    <div class="menu-botton"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const active_index = ref(0)
+</script>
+
+<style scoped lang="scss">
+.my-info-menu-bar-container {
+  position: fixed;
+  right: 15px;
+  top: 15px;
+  width: 70px;
+  height: calc(100vh - 30px);
+  background-color: #202029;
+  display: flex;
+  flex-direction: column;
+  color: #8c8c8e;
+  .menu-top {
+    width: 70px;
+    height: 70px;
+    background-color: #24242f;
+  }
+  .menu-center {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      width: 0px;
+    }
+    .menu-item {
+      padding: 18px 0;
+      box-sizing: border-box;
+      text-align: center;
+      flex-shrink: 0;
+      font-size: 11px;
+    }
+  }
+  .menu-botton {
+    width: 70px;
+    height: 140px;
+    background-color: #252530;
+  }
+}
+.my-info-menu-bar-top {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 30px;
+  background-color: #202029;
+  display: flex;
+  flex-direction: column;
+  color: #8c8c8e;
+}
+
+@media (max-width: 920px) {
+  .my-info-menu-bar-container {
+    opacity: 0 !important;
+  }
+}
+@media (min-width: 920px) {
+  .my-info-menu-bar-top {
+    opacity: 1 !important;
+  }
+}
+</style>
--- src/views/BlogsHomePage/index.vue
+++ src/views/BlogsHomePage/index.vue
@@ -2,7 +2,7 @@
  * @Author: 微生
  * @Date: 2024-04-03 11:51:48
  * @LastEditors: WeiSheng 842469165@qq.com
- * @LastEditTime: 2024-04-04 14:01:26
+ * @LastEditTime: 2024-04-04 16:18:26
  * @FilePath: /blogs-G/src/views/BlogsHomePage/index.vue
  * @Description: 
  * 
@@ -17,7 +17,8 @@
           <div v-for="item in 100">测试文案测试文案测试文案测试文案测试文案测试文案测试文案测试文案</div>
         </div>
       </div>
-      <div class="my-info-menu-bar transform-55">
+      <MyInfoMenuBar />
+      <!-- <div class="my-info-menu-bar transform-55">
         <div class="menu-top"></div>
         <div class="menu-center">
           <div class="menu-item" :class="{ 'act-item': active_index === index }" v-for="(item, index) in 20">
@@ -25,7 +26,7 @@
           </div>
         </div>
         <div class="menu-botton"></div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -33,9 +34,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import MyInfoSideBar from './components/MyInfoSideBar.vue'
-
-const show = ref(true)
-const active_index = ref(0)
+import MyInfoMenuBar from './components/MyInfoMenuBar.vue'
+// const show = ref(true)
+// const active_index = ref(0)
 </script>
 
 <style scoped lang="scss">
@@ -57,54 +58,6 @@ const active_index = ref(0)
     // overflow-y: hidden;
     // 禁用滚动
 
-    .my-info-menu-bar {
-      position: fixed;
-      right: 15px;
-      top: 15px;
-      width: 70px;
-      height: calc(100vh - 30px);
-      background-color: #202029;
-      display: flex;
-      flex-direction: column;
-      color: #8c8c8e;
-      .menu-top {
-        width: 70px;
-        height: 70px;
-        background-color: #24242f;
-      }
-      .menu-center {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        overflow-y: scroll;
-        &::-webkit-scrollbar {
-          width: 0px;
-        }
-        .menu-item {
-          padding: 18px 0;
-          box-sizing: border-box;
-          text-align: center;
-          flex-shrink: 0;
-          font-size: 11px;
-        }
-      }
-      .menu-botton {
-        width: 70px;
-        height: 140px;
-        background-color: #252530;
-      }
-    }
-    .my-info-menu-bar-top {
-      position: fixed;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 30px;
-      background-color: #202029;
-      display: flex;
-      flex-direction: column;
-      color: #8c8c8e;
-    }
     .blogs-content {
       // background-color: orange;
       height: calc(100vh - 30px);
